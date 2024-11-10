132386f8e6bb92f8777075ae0862c92297a809e2
Marius Drumea <marius.drumea@endava.com>
2024-11-10 13:52:23 +0200

    Added Year 2022 Day 6

---
+++ Year2022/Day06/Challenge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2022.Day06
+{
+	internal class Challenge : BaseDayChallenge, IDayChallenge
+	{
+		public void Part1(Source source)
+		{
+			var lines = LoadSource(source);
+
+			var line = lines[0];
+
+			int count = 0;
+			for (int i = 4; i < line.Length; i++)
+			{
+				if (line.Substring(i-4, 4).ToHashSet().Count() == 4)
+				{
+					count = i;
+					break;
+				}
+			}
+
+			Console.WriteLine($"Characters processed until start of message: {count}");
+		}
+
+		public void Part2(Source source)
+		{
+			var lines = LoadSource(source);
+
+			var line = lines[0];
+
+			int count = 0;
+			for (int i = 14; i < line.Length; i++)
+			{
+				if (line.Substring(i - 14, 14).ToHashSet().Count() == 14)
+				{
+					count = i;
+					break;
+				}
+			}
+
+			Console.WriteLine($"Characters processed until start of message: {count}");
+		}
+	}
+}
