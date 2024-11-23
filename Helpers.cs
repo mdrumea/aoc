0efad3775c936286c7f57855472bc3e644b10fb3
Marius Drumea <marius.drumea@endava.com>
2024-11-23 14:22:11 +0200

    Added Year 2022 Day 8. Added matrix helpers.

---
+++ Helpers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+	internal static class Helpers
+	{
+		public static T[,] ToMatrix<T>(this string[] lines)
+		{
+			T[,] grid = new T[lines.Length, lines[0].Length];
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				for (int j = 0; j < lines[0].Length; j++)
+				{
+					if (typeof(T) == typeof(int))
+					{
+						grid[i, j] = (T)(object)int.Parse(lines[i][j].ToString());
+					}
+					else if (typeof(T) == typeof(string))
+					{
+						grid[i, j] = (T)(object)lines[i][j].ToString();
+					}
+					else
+					{
+						throw new NotImplementedException();
+					}
+
+				}
+			}
+
+			return grid;
+		}
+
+		public static int RowCount<T>(this T[,] array) => array.GetLength(0);
+
+		public static int ColumnCount<T>(this T[,] array) => array.GetLength(1);
+
+		public static T[] GetRow<T>(this T[,] array, int row)
+		{
+			T[] result = new T[array.ColumnCount()];
+
+			for (int i = 0; i < array.ColumnCount(); i++)
+			{
+				result[i] = array[row, i];
+			}
+
+			return result;
+		}
+
+		public static T[] GetColumn<T>(this T[,] array, int column)
+		{
+			T[] result = new T[array.RowCount()];
+
+			for (int i = 0; i < array.RowCount(); i++)
+			{
+				result[i] = array[i, column];
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Year2022/Day08/Challenge.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CommunityToolkit.HighPerformance;
+
+namespace AdventOfCode.Year2022.Day08
+{
+	internal class Challenge : BaseDayChallenge, IDayChallenge
+	{
+		public void Part1(Source source)
+		{
+			var lines = LoadSource(source);
+
+			//int[,] grid = new int[lines.Length, lines[0].Length];
+
+			int[,] grid = lines.ToMatrix<int>();
+
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				for (int j = 0; j < lines[0].Length; j++)
+				{
+					grid[i, j] = int.Parse(lines[i][j].ToString());
+				}
+			}
+
+
+			int count = 2 * (grid.RowCount() + grid.ColumnCount() - 2); // exterior trees
+
+
+			for (int i = 1; i < grid.RowCount() - 1; i++)
+			{
+				for (int j = 1; j < grid.ColumnCount() - 1; j++)
+				{
+					var isVisibleW = grid.GetRow(i).ToArray().Select((t, idx) => new { t, idx }).Where(pair => 0 <= pair.idx && pair.idx < j).All(pair => pair.t < grid[i, j]);
+					var isVisibleE = grid.GetRow(i).ToArray().Select((t, idx) => new { t, idx }).Where(pair => j < pair.idx && pair.idx <= grid.ColumnCount() -1).All(pair => pair.t < grid[i, j]);
+					var isVisibleN = grid.GetColumn(j).ToArray().Select((t, idx) => new { t, idx }).Where(pair => 0 <= pair.idx && pair.idx < i).All(pair => pair.t < grid[i, j]);
+					var isVisibleS = grid.GetColumn(j).ToArray().Select((t, idx) => new { t, idx }).Where(pair => i < pair.idx && pair.idx <= grid.RowCount() - 1).All(pair => pair.t < grid[i, j]);
+
+					if (isVisibleW || isVisibleE || isVisibleN || isVisibleS)
+					{
+						count++;
+					}
+				}
+			}
+
+			Console.WriteLine($"Number of visible trees: {count}");
+		}
+
+		public void Part2(Source source)
+		{
+			var lines = LoadSource(source);
+
+			int[,] grid = new int[lines.Length, lines[0].Length];
+
+			for (int i = 0; i < lines.Length; i++)
+			{
+				for (int j = 0; j < lines[0].Length; j++)
+				{
+					grid[i, j] = int.Parse(lines[i][j].ToString());
+				}
+			}
+
+
+			int maxScenicScore = 0;
+
+
+			for (int i = 0; i <= grid.RowCount() - 1; i++)
+			{
+				for (int j = 0; j <= grid.ColumnCount() - 1; j++)
+				{
+					var scoreW = 0;
+					var treesW = grid.GetRow(i).ToArray();
+					for (int k = j - 1; k >= 0; k--)
+					{
+						if (treesW[k] < treesW[j])
+						{
+							scoreW++;
+						}
+						else
+						{
+							scoreW++;
+							break;
+						}
+					}
+
+					var scoreE = 0;
+					var treesE = grid.GetRow(i).ToArray();
+					for (int k = j +1 ; k <= grid.ColumnCount() - 1; k++)
+					{
+						if (treesE[k] < treesE[j])
+						{
+							scoreE++;
+						}
+						else
+						{
+							scoreE++;
+							break;
+						}
+					}
+
+
+					var scoreN = 0;
+					var treesN = grid.GetColumn(j).ToArray();
+					for (int k = i - 1; k >= 0; k--)
+					{
+						if (treesN[k] < treesN[i])
+						{
+							scoreN++;
+						}
+						else
+						{
+							scoreN++;
+							break;
+						}
+					}
+
+					var scoreS = 0;
+					var treesS = grid.GetColumn(j).ToArray();
+					for (int k = i + 1; k <= grid.RowCount() - 1; k++)
+					{
+						if (treesS[k] < treesS[i])
+						{
+							scoreS++;
+						}
+						else
+						{
+							scoreS++;
+							break;
+						}
+					}
+
+					maxScenicScore = Math.Max(maxScenicScore, scoreS * scoreN * scoreE * scoreW);
+				}
+			}
+
+			Console.WriteLine($"Maximum scenic score: {maxScenicScore}");
+		}
+	}
+}
